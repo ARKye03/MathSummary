ec1463bd7b414074efa27b5280df14c7489aa171
ARKye03 <rafa03-dev@proton.me>
2024-09-01 13:30:09 -0400

    chore: Update .gitignore and flake.nix
    Add package to the flake

--- flake.nix
+++ flake.nix
@@ -20,6 +20,22 @@
         in
         {
           devShells.default = shell;
+          packages.default = pkgs.stdenv.mkDerivation {
+            name = "pdf";
+            version = "1.1.test";
+            nativeBuildInputs = with pkgs.buildPackages; [
+              typst
+            ];
+            src = ./src;
+
+            buildPhase = /* bash */ ''
+              mkdir -p $TMPDIR/typst
+              cd $TMPDIR/typst
+              typst compile $src/main.typ $TMPDIR/typst/main.pdf
+              mkdir -p $out/bin
+              cp $TMPDIR/typst/main.pdf $out/bin/main.pdf
+            '';
+          };
         }
       );
 }
